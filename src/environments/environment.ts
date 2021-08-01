0935f46b8b25d4e2008b01db5a89e2bd7ebdcfe9
George-ongaya1git config --global user.email georgeongaya94.gmail.comgit config --golabl user.name George-ongaya1 <georgeongaya94@gmail.com>
2021-08-01 12:50:30 +0300

    added the github token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false
+  
 };
-
+//git hub token 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
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
